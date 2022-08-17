70b622b1e743c6f12aed444570247c67633f12fb
Shubham Borkar <shubham_borkar@outlook.com>
2022-08-17 14:28:20 +0530

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update -y
 sudo apt install default-jre -y
-sudo apt install tomcat9 tomcat9-admin -y 
-sudo systemctl start tomcat9
-sudo systemctl enable tomcat9
-sudo systemctl status tomcat9
+sudo apt install tomcat8 tomcat8-admin -y 
+sudo systemctl start tomcat8
+sudo systemctl enable tomcat8
+sudo systemctl status tomcat8
