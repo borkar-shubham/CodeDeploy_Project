e7b0c81c690d829b96330c7b5a2c0aeab7c7bf07
Shubham Borkar <shubham_borkar@outlook.com>
2022-08-17 14:02:55 +0530

    Update start_application.sh

--- start_application.sh
+++ start_application.sh
@@ -1,4 +1,4 @@
 # sudo cp student.war /var/lib/tomcat9/webapps/
 sudo chmod +x /var/lib/tomcat9/webapps/student*
-sudo chown -R /var/lib/tomcat9/webapps/student*
+sudo chown -R tomcat:tomcat /var/lib/tomcat9/webapps/student*
 sudo service tomcat9 start
