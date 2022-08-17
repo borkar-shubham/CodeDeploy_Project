8bf0c14e5e8e414fff786ee1fe46340182383481
Shubham Borkar <shubham_borkar@outlook.com>
2022-08-17 15:46:12 +0530

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 sudo apt update -y
-sudo apt install default-jre -y
+sudo apt install default-jre default-jdk -y
 # sudo apt install tomcat9 tomcat8-admin -y 
 # sudo systemctl start tomcat9
 # sudo systemctl enable tomcat9
 # sudo systemctl status tomcat9
-wget -c http://launchpadlibrarian.net/366407131/tomcat8_8.5.30-1ubuntu1_all.deb http://launchpadlibrarian.net/366407127/tomcat8-common_8.5.30-1ubuntu1_all.deb http://launchpadlibrarian.net/366407125/libtomcat8-java_8.5.30-1ubuntu1_all.deb
-sudo apt install ./libtomcat8-java_8.5.30-1ubuntu1_all.deb ./tomcat8_8.5.30-1ubuntu1_all.deb ./tomcat8-common_8.5.30-1ubuntu1_all.deb -y
+# wget -c http://launchpadlibrarian.net/366407131/tomcat8_8.5.30-1ubuntu1_all.deb http://launchpadlibrarian.net/366407127/tomcat8-common_8.5.30-1ubuntu1_all.deb http://launchpadlibrarian.net/366407125/libtomcat8-java_8.5.30-1ubuntu1_all.deb
+# sudo apt install ./libtomcat8-java_8.5.30-1ubuntu1_all.deb ./tomcat8_8.5.30-1ubuntu1_all.deb ./tomcat8-common_8.5.30-1ubuntu1_all.deb -y
+
+curl -O https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.82/bin/apache-tomcat-8.5.82.tar.gz
+tar -xvfz apache-tomcat-8.5.82.tar.gz -C /opt/
+mv /opt/apache-tomcat-8.5.82 /opt/tomcat
+./opt/tomcat/bin/catalina.sh start 
