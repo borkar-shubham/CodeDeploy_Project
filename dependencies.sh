a2da68a15eb212117a172a953b9be2521ff772e1
Shubham Borkar <shubham_borkar@outlook.com>
2022-08-17 14:36:28 +0530

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -6,4 +6,4 @@ sudo apt install default-jre -y
 # sudo systemctl enable tomcat9
 # sudo systemctl status tomcat9
 wget -c http://launchpadlibrarian.net/366407131/tomcat8_8.5.30-1ubuntu1_all.deb http://launchpadlibrarian.net/366407127/tomcat8-common_8.5.30-1ubuntu1_all.deb http://launchpadlibrarian.net/366407125/libtomcat8-java_8.5.30-1ubuntu1_all.deb
-sudo apt install ./libtomcat8-java_8.5.30-1ubuntu1_all.deb ./tomcat8_8.5.30-1ubuntu1_all.deb ./tomcat8-common_8.5.30-1ubuntu1_all.deb
+sudo apt install ./libtomcat8-java_8.5.30-1ubuntu1_all.deb ./tomcat8_8.5.30-1ubuntu1_all.deb ./tomcat8-common_8.5.30-1ubuntu1_all.deb -y
